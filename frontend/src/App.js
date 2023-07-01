43e1603e4855ec311e29df7bbb4d99111862a411
Nikhil-Shahare <108420298+Nikhil-Shahare@users.noreply.github.com>
2023-07-01 17:09:12 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -46,7 +46,7 @@ return (
    </div>
         <div className="col-lg-8 background">
         
-        {valid!=="false"?alert(valid):""}
+        {valid!=="false"?alert(valid):alert("notification")}
           {/*Component Contain all the movies name */}
           <Movie  />
           {/*Component Contain all the movies time */}
