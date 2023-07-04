cb08814b5617e47b64d0e5423500a5859e527e4e
Nikhil-Shahare <108420298+Nikhil-Shahare@users.noreply.github.com>
2023-07-04 22:31:45 +0530

    Update Movie.jsx

--- frontend/src/components/Movie.jsx
+++ frontend/src/components/Movie.jsx
@@ -1,39 +1,81 @@
-import React, { memo } from 'react';
+import React, { memo } from 'react'
 import { useSelector, useDispatch } from "react-redux";
 import { setMovie } from '../redux/bookmovieSlice';
-import "./movie.css";
-
+import "./movie.css"
 const Movie = () => {
+
   const dispatch = useDispatch();
 
-  // Event handler for updating the selected movie
   const updatemoviebookname = (e) => {
-    dispatch(setMovie(e.target.value)); // Dispatch the action to update the selected movie
+    dispatch(setMovie(e.target.value));
+  
+ 
   };
 
   return (
-    <div className='border rounded border-dark mb-1'>
-      <h3>Select a Movie</h3>
-      <div className="movie">
-        {/* Radio buttons for displaying movie names */}
-        <input
-          type="radio"
-          class="btn-check"
-          name="movie"
-          value="Suraj par mangal bhari"
-          id="1"
-          autocomplete="off"
-          onChange={(e) => updatemoviebookname(e)}
-        />
-        <label class="btn btn-outline-danger" for="1">
-          Suraj par mangal bhari
-        </label>
+    <div className='cover '>
+  
+        
+          <h3>Select a Movie</h3>
+          <div className="movie">
+            {/*radio box for showing Movies name */}
+            <input
+              type="radio"
+              class="btn-check"
+              name="movie"
+              value="Suraj par mangal bhari"
+              id="1"
+              
+              autocomplete="off"
+              onChange={(e)=>updatemoviebookname(e)}
 
-        {/* Repeat the same structure for other movie options */}
+            />
+            <label class="btn btn-outline-danger" for="1">
+              Suraj par mangal bhari
+            </label>
 
-      </div>
+            <input
+              type="radio"
+              class="btn-check"
+              name="movie"
+              id="2"
+             value="Tenet"
+              autocomplete="off"
+              onChange={(e)=>updatemoviebookname(e)}
+            />
+            <label class="btn btn-outline-danger" for="2">
+              Tenet
+            </label>
+
+            <input
+             type="radio"
+              class="btn-check"
+              name="movie"
+              id="3"
+              value="The war with grandpa"
+              autocomplete="off"
+              onChange={(e)=>updatemoviebookname(e)}
+            />
+            <label class="btn btn-outline-danger" for="3">
+              The war with grandpa
+            </label>
+
+            <input
+             type="radio"
+              class="btn-check"
+              name="movie"
+              id="4"
+              value="The perosnal history of David Copperfield"
+              autocomplete="off"
+              onChange={(e)=>updatemoviebookname(e)}
+            />
+            <label class="btn btn-outline-danger" for="4">
+              The perosnal history of David Copperfield
+            </label>
+          </div>
+          
     </div>
-  );
-};
+  )
+}
 
-export default Movie;
+export default Movie
